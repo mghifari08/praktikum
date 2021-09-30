9378c268dc6a0909eb3c79d62641b83b61ab5cf0
muhammad <muhammadghifari927@gmail.com>
2021-09-29 20:10:18 -0700

    HelloWorld.java

---
+++ DemoGit/HelloWorld.java
@@ -0,0 +1,9 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+	    System.out.println("Nama: Ghifari!");
+		System.out.println("NIM: D3IF6706213017");
+		System.out.println("Kelas: 45-GAB1");
+	}
+}
+	
